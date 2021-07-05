7c0820cc9ec221433d1641bc4a942a855ce51afa
Supreetmalhi <malhisupreet40@gmail.com>
2021-07-05 19:44:57 -0400

    Initial commit

---
+++ Accounting Software Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Accounting_Software_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we’re \"accounting\" on you!");
+        }
+    }
+}
